a4f8977320fc39abda9c9a53379203ae09172694
lukereynoldspi <lukereynoldspi@gmail.com>
2022-01-23 14:17:55 -0800

    made some progress on all parts of the project?

--- mush.c
+++ mush.c
@@ -1,3 +1,5 @@
+//~Luke Reynolds~
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,6 +16,7 @@ int main(int argc, char *argv[])
     char *token;
     char *s[128];
     int i = 0;
+
     if ((token = strtok(buf, " \t\n\r")) != NULL) {
         do {
             printf("%s\n", token);
@@ -22,5 +25,26 @@ int main(int argc, char *argv[])
         } while ((token = strtok(NULL, " \t\n\r")) != NULL);
     }
     s[i] = '\0';
+
+    if (strcmp(s[0], "cd") == 0) {
+        int second_arg = chdir(s[1]);
+        if (second_arg == -1) {
+            perror("Error");
+        }
+    }
+    else if (strcmp(s[0], "exit") == 0) {
+        exit(0);
+    }
+     
     execvp(s[0], argv);
+    pid_t pid = fork();
+    if (pid == 0) {
+        execvp(s[0], s);
+        perror("exec");
+        exit(1);
+    }
+    else {
+        wait(NULL);
+    }
+
 }
