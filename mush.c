9129bf65726fc170d50788a70086c6db354eef17
lukereynoldspi <lukereynoldspi@gmail.com>
2022-01-23 15:10:16 -0800

    Added print statement at start of program

--- mush.c
+++ mush.c
@@ -7,44 +7,52 @@
 #include <sys/wait.h>
 int main(int argc, char *argv[])
 {
-    char buf[2048];
+    printf("Reynoodles OS v. 1.0\n\n");
 
-    printf("Enter something: ");
-    fflush(stdout);
-    fgets(buf, sizeof buf, stdin);
+    while (1) {
+        /// Part 1
+        char buf[2048];
 
-    char *token;
-    char *s[128];
-    int i = 0;
+        printf("🔥Luke\'s Awesome Shell 🔥$ ");
+        fflush(stdout);
+        fgets(buf, sizeof buf, stdin);
 
-    if ((token = strtok(buf, " \t\n\r")) != NULL) {
-        do {
-            printf("%s\n", token);
-            s[i] = token;
-            i++;
-        } while ((token = strtok(NULL, " \t\n\r")) != NULL);
-    }
-    s[i] = '\0';
+        char *token;
+        char *s[128];
+        int i = 0;
 
-    if (strcmp(s[0], "cd") == 0) {
-        int second_arg = chdir(s[1]);
-        if (second_arg == -1) {
-            perror("Error");
+        if ((token = strtok(buf, " \t\n\r")) != NULL) {
+            do {
+                s[i] = token;
+                i++;
+            } while ((token = strtok(NULL, " \t\n\r")) != NULL);
+        }
+        s[i] = '\0';
+
+        /// Part 3
+        if (strcmp(s[0], "cd") == 0) {
+            int second_arg = chdir(s[1]);
+            if (second_arg == -1) {
+                perror("Error");
+            }
+        }
+
+        /// Part 4
+        else if (strcmp(s[0], "exit") == 0) {
+            exit(0);
+        }
+
+        /// Part 2
+        else {
+            pid_t pid = fork();
+            if (pid == 0) {
+                execvp(s[0], s);
+                perror("exec");
+                exit(1);
+            }
+            else {
+                wait(NULL);
+            }
         }
     }
-    else if (strcmp(s[0], "exit") == 0) {
-        exit(0);
-    }
-     
-    execvp(s[0], argv);
-    pid_t pid = fork();
-    if (pid == 0) {
-        execvp(s[0], s);
-        perror("exec");
-        exit(1);
-    }
-    else {
-        wait(NULL);
-    }
-
 }
